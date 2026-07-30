75e9c7ff8166ec66d2f2cf6e2325cb77a717ba0a
vitaliy abrosimov <vitalikabrosimov00@gmail.com>
2026-07-30 16:42:30 +0300

    добавлен docker-compose.yml для базы данных

--- src/db.ts
+++ src/db.ts
@@ -2,8 +2,10 @@ import {PrismaClient} from "./generated/prisma/client.js";
 import {PrismaPg} from "@prisma/adapter-pg";
 import pkg from 'pg';
 
+const dbUrl = `postgresql://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@localhost:5432/${process.env.DATABASE_DATABASE}?schema=public`
+
 const { Pool } = pkg;
-const pool = new Pool({connectionString: process.env.DATABASE_URL});
+const pool = new Pool({connectionString: dbUrl});
 const adapter = new PrismaPg(pool);
 
 export const db = new PrismaClient({adapter})
